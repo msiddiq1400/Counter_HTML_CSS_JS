a8327431d38222ac998ca42ec9e1864b934f2204
siddiq <msiddiq1400@gmail.com>
2023-02-17 00:42:28 +0500

    some animation added

--- index.js
+++ index.js
@@ -3,20 +3,29 @@ let hours = document.getElementById("hours");
 let minutes = document.getElementById("minutes");
 let seconds = document.getElementById("seconds");
 
+let secondsCard = document.getElementById("seconds-card");
+let minutesCard = document.getElementById("minutes-card");
+
 setInterval(() => {
   //setting seconds
   let mySec = parseInt(seconds.innerText);
   if (mySec <= 10 && mySec > 0) {
     seconds.innerText = `0${parseInt(seconds.innerText) - 1}`;
+    secondsCard.style.animation = "animate 1s linear infinite";
+    minutesCard.style.animation = "";
   } else if (mySec >= 10) {
     seconds.innerText = parseInt(seconds.innerText) - 1;
+    secondsCard.style.animation = "animate 1s linear infinite";
+    minutesCard.style.animation = "";
   } else if (mySec < 1) {
     //setting mins
     let myMin = parseInt(minutes.innerText);
     if (myMin <= 10 && myMin > 0) {
       minutes.innerText = `0${parseInt(minutes.innerText) - 1}`;
+      minutesCard.style.animation = "animate 1s linear infinite";
     } else if (myMin >= 10) {
       minutes.innerText = parseInt(minutes.innerText) - 1;
+      minutesCard.style.animation = "animate 1s linear infinite";
     } else if (myMin < 1) {
       //setting hours
       let myHour = parseInt(hours.innerText);
@@ -29,7 +38,9 @@ setInterval(() => {
         hours.innerText = "24";
       }
       minutes.innerText = "59";
+      minutesCard.style.animation = "animate 1s linear infinite";
     }
     seconds.innerText = "59";
+    secondsCard.style.animation = "animate 1s linear infinite";
   }
 }, 1000);
